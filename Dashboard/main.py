40cc642e635d53a4717aa6cfa47c788d6992f514
adhoc12 <arthurmilner01@gmail.com>
2023-02-08 16:16:29 +0000

    Added templates, basic sidebar, etc.

--- Dashboard/main.py
+++ Dashboard/main.py
@@ -6,9 +6,19 @@ app = Flask(__name__)
 
 @app.route("/home") #Route for the about us page
 @app.route("/")        
-def aboutus():
+def home():
    print("Home")
-   return render_template("base.html")
+   return render_template("home.html")
+
+@app.route("/players") #Route for the players page        
+def players():
+   print("Players")
+   return render_template("players.html")
+
+@app.route("/clubs") #Route for the clubs page        
+def clubs():
+   print("Clubs")
+   return render_template("clubs.html")
 
 if __name__ == "__main__":
    app.run(debug = True) #will run the flask app
