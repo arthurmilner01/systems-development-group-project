7efab01485c9e2a78aed1da2d6daa8c5072fce9b
adhoc12 <arthurmilner01@gmail.com>
2023-03-01 13:39:09 +0000

    removed unecessary code

--- Dashboard/main.py
+++ Dashboard/main.py
@@ -4,10 +4,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-def get_db_connection():
-    conn = sqlite3.connect('MoneyballDB.db')
-    conn.row_factory = sqlite3.Row
-    return conn
 @app.route("/home") #Route for the about us page
 @app.route("/")        
 def home():
