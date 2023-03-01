4104e18b3ed8d8103f7236e2a94bbe8a8180e529
JudPP <judahtilbury@outlook.com>
2023-03-01 11:17:37 +0000

    Big w

--- Dashboard/main.py
+++ Dashboard/main.py
@@ -1,14 +1,6 @@
 from DatabaseAccess import *
 from flask import Flask, render_template
 
-
-
-playerHeadings = ['Player Name', 'Date of Birth', 'Gender', 'Date Signed-Up', 'Current Team', 'Team Location', 'Team Manager', 'Salary (£k/Week)', 'Start of Contract', 'Contract Duration', 'Games Played This Year', 'Games Won', 'Future Games']
-clubHeadings = ['Team Name', 'Team Location', 'Team Manager']
-
-    
-
-
 app = Flask(__name__)
 
 @app.route("/home") #Route for the about us page
@@ -19,13 +11,15 @@ def home():
 
 @app.route("/players") #Route for the players page        
 def players():
+   playerHeadings = ['Player Name', 'Date of Birth', 'Gender', 'Date Signed-Up', 'Current Team', 'Team Location', 'Team Manager', 'Salary (£k/Week)', 'Start of Contract', 'Contract Duration', 'Games Played This Year', 'Games Won', 'Future Games']
    print("Players")
-   return render_template("players.html")
+   return render_template("players.html", playerHeadings)
 
 @app.route("/clubs") #Route for the clubs page        
 def clubs():
+   clubHeadings = ['Team Name', 'Team Location', 'Team Manager']
    print("Clubs")
-   return render_template("clubs.html")
+   return render_template("clubs.html", clubHeadings)
 
 if __name__ == "__main__":
    app.run(debug = True) #will run the flask app
