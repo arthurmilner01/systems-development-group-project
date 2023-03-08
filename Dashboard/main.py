bfa24076aedcdd6726f5d107b96bb27daf017026
j2-cauvy-foster <joseph2.cauvy-foster@live.uwe.ac.uk>
2023-03-08 10:02:31 +0000

    home graphs working

--- Dashboard/main.py
+++ Dashboard/main.py
@@ -41,7 +41,7 @@ def getWeeksLeftInContract(playerStartOfContract, playerContractDuration):
    playerWeeksLeftInContract = playerWeeksInContract - playerWeeksPlayedOfContract
    return playerWeeksLeftInContract
 
-@app.route("/home") #Route for the about us page
+@app.route("/home") #Route for the home page
 @app.route("/")        
 def home():
    print("Home")
@@ -50,18 +50,15 @@ def home():
       cur.execute('''SELECT * FROM Players ORDER BY RANDOM() LIMIT 5''')
       data = cur.fetchall()
    conn.close()
-   #Get current date in desired format
-   currentDate = datetime.now()
-   currentDate = currentDate.strftime("%d/%m/%Y")
-   currentDate = datetime.strptime(currentDate, "%d/%m/%Y")
    trendingPlayers = []
    for i in range(5):
       playerWeeksLeftInContract = getWeeksLeftInContract(data[i][7], data[i][8])
-      trendingPlayers.append(calculatePrices(data[i][6], data[i][10], playerWeeksLeftInContract, data[i][9], data[i][11]))
+      trendingPlayers.append(calculatePrices((data[i][6])*1000, data[i][10], playerWeeksLeftInContract, data[i][9], data[i][11]))
       for j in range(len(trendingPlayers[i])):
          trendingPlayers[i][j] = round(trendingPlayers[i][j], 2)
    print(trendingPlayers)
-   return render_template("home.html", data = trendingPlayers[0], playerToWatch = data[0])
+   print(trendingPlayers[0])
+   return render_template("home.html", name1 = data[0][1], name2 = data[1][1], name3 = data[2][1], name4 = data[3][1], name5 = data[4][1], player1 = trendingPlayers[0], player2 = trendingPlayers[1], player3 = trendingPlayers[2], player4 = trendingPlayers[3], player5 = trendingPlayers[4], playerToWatch = data[0])
 
 @app.route("/players") #Route for the players page        
 def players():
