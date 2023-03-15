bdc3dc2c3a2361948994fc3ebea3170712633857
t2-diclaudio <tommy2.diclaudio@live.uwe.ac.uk>
2023-03-15 11:47:01 +0000

    Test progress

--- Dashboard/testcode.py
+++ Dashboard/testcode.py
@@ -8,6 +8,18 @@ class TestDatabase(unittest.TestCase):
     def test_connection(self):
         self.assertTrue(conn)
 
+class TestMain(unittest.TestCase):
+
+    # Test for getting weeks left
+    def test_getweeksleft():
+        pass
+
+    # Test for Market Value calculation being correct
+    def test_calculation(self):
+
+        success = self.model = calculatePrices(50, 11, 40, 22, "WWLWD") #Salary, Games won, Weeks left, Games Played, Future games
+        self.assertEqual(success, 1200000)
+        # self.assertEqaul(playerPrices, ) #what it equals
 
 class TestLogin(unittest.TestCase):
     
