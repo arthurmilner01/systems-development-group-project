70837d940b0ed5f92dd808570fb265ac306066c3
t2-diclaudio <tommy2.diclaudio@live.uwe.ac.uk>
2023-03-15 10:37:47 +0000

    Unit test start

---
+++ Dashboard/Unit Tests/testcode.py
@@ -0,0 +1,22 @@
+import unittest
+from DatabaseAccess import *
+from main import *
+
+
+class TestDatabase(unittest.TestCase):
+
+    def test_connection(self):
+        self.assertTrue(conn)
+
+
+class TestLogin(unittest.TestCase):
+    
+    def test_login(self):
+        pass
+
+
+class TestAdmin(unittest.TestCase):
+
+    def add_player(self):
+        pass
+
