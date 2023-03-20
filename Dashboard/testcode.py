4cb38cc0540f419ca8969b010acf110d54f0b181
t2-diclaudio <tommy2.diclaudio@live.uwe.ac.uk>
2023-03-20 12:47:22 +0000

    Main Tests

--- Dashboard/testcode.py
+++ Dashboard/testcode.py
@@ -3,28 +3,48 @@ from DatabaseAccess import *
 from main import *
 
 
+
 class TestDatabase(unittest.TestCase):
 
     def test_connection(self):
         self.assertTrue(conn)
 
-class TestMain(unittest.TestCase):
+
+class TestMainProgram(unittest.TestCase):
 
     # Test for getting weeks left
-    def test_getweeksleft():
-        pass
+    def test_weeksleft(self):
+
+        self.weeksleft = getWeeksLeftInContract(playerStartOfContract = "01/01/2019", playerContractDuration = 5)
+
+        print(self.weeksleft)
+
+        expected = 41
+
+        self.assertEqual(self.weeksleft, expected)
 
     # Test for Market Value calculation being correct
-    def test_calculation(self):
+    def test_marketcalculation(self):
+        
+        self.marketvalue = calculatePrices(playerSalary = 100000, playerGamesWon = 10, playerWeeksLeftInContract = 10, playerGamesPlayedThisYear = 20, playerFutureGames = "WWWLL")
 
-        success = self.model = calculatePrices(50, 11, 40, 22, "WWLWD") #Salary, Games won, Weeks left, Games Played, Future games
-        self.assertEqual(success, 1200000)
-        # self.assertEqaul(playerPrices, ) #what it equals
+        print(self.marketvalue[0])
+
+        expected = [500000, 471428.5714285715, 436363.63636363635, 395652.17391304346, 325000.0, 260000.0] # Salary, FG1, FG2, FG3, FG4, FG5
+
+        self.assertEqual(self.marketvalue, expected)
+
+class TestRoutes(unittest.TestCase):
+    
+    pass
 
 class TestLogin(unittest.TestCase):
     
     def test_login(self):
-        pass
+        
+        #self, 'admin@gmail.com', 'admin123'
+
+        success = self.model = login()
 
 
 class TestAdmin(unittest.TestCase):
